e53bea315a7f3d7fc275046554c67b6a3448aac2
차재훈 <cjh8948@naver.com>
2018-06-02 19:20:30 +0900

    리소스 추가, Splash Activity 수정
    Drawable image file 추가, Color.xml, String.xml 수정 Splash Activity 수정

--- nogang/qtum-android/app/src/main/java/org/qtum/wallet/ui/activity/splash_activity/SplashActivity.java
+++ nogang/qtum-android/app/src/main/java/org/qtum/wallet/ui/activity/splash_activity/SplashActivity.java
@@ -19,33 +19,33 @@ import butterknife.BindView;
 
 public class SplashActivity extends BaseActivity implements SplashActivityView {
     private SplashActivityPresenter presenter;
-    private static final int LAYOUT = R.layout.lyt_splash; //Todo: 처음 앱 기동시 표시할 레이아웃 작성
+    private static final int LAYOUT = R.layout.activity_splash; //Todo: 처음 앱 기동시 표시할 레이아웃 작성
 
-    @BindView(R.id.ic_app_logo)
-    AppCompatImageView appLogo;
+//    @BindView(R.id.ic_app_logo)
+//    AppCompatImageView appLogo;
 
-    private int appLogoHeight = 0;
+//    private int appLogoHeight = 0;
 
     @Override
     public void initializeViews() {
-        recolorStatusBar(R.color.background);
+//        recolorStatusBar(R.color.background);
 
         // Todo : Splash 화면이 로딩된 뒤 DoTransaction(에니메이션 코드) 을 수행 하기 위한 코드 appLogo 의 사이즈가 0이라면 에니메이션 수행하지 않고
         // 로딩 된뒤 수행하도록 하기 위한것 응용하여 작성
 
-        if (appLogo.getHeight() == 0) {
-            appLogo.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                @Override
-                public void onGlobalLayout() {
-                    appLogo.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                    appLogoHeight = (appLogoHeight == 0) ? appLogo.getHeight() : appLogoHeight;
-                    DoTransition();
-                }
-            });
-        } else {
-            appLogoHeight = (appLogoHeight == 0) ? appLogo.getHeight() : appLogoHeight;
-            DoTransition();
-        }
+//        if (appLogo.getHeight() == 0) {
+//            appLogo.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//                @Override
+//                public void onGlobalLayout() {
+//                    appLogo.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+//                    appLogoHeight = (appLogoHeight == 0) ? appLogo.getHeight() : appLogoHeight;
+//                    DoTransition();
+//                }
+//            });
+//        } else {
+//            appLogoHeight = (appLogoHeight == 0) ? appLogo.getHeight() : appLogoHeight;
+//            DoTransition();
+//        }
 
         //앱 실행 후 2초 뒤에 MainActivity 수행하도록함.
         startHandler = new Handler();
@@ -53,9 +53,9 @@ public class SplashActivity extends BaseActivity implements SplashActivityView {
 
     }
 
-    private void DoTransition() {
-        //Todo : 에니메이션 코드 작성
-    }
+//    private void DoTransition() {
+//        //Todo : 에니메이션 코드 작성
+//    }
 
     @Override
     protected void updateTheme() {
@@ -90,11 +90,11 @@ public class SplashActivity extends BaseActivity implements SplashActivityView {
         //Todo : 에니메이션 실행 코드 작성
     }
 
-    public void recolorStatusBar(int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(ContextCompat.getColor(getContext(), color));
-        }
-    }
+//    public void recolorStatusBar(int color) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            getWindow().setStatusBarColor(ContextCompat.getColor(getContext(), color));
+//        }
+//    }
 
     @Override
     protected void createPresenter() {
